cf79fd28370be55b2c9cd222cca15189fa9b8d01
Jérémy Levallois <jeremy.levallois@adobe.com>
2022-01-21 12:34:55 +0100

    moving to shader version 410 core profile

--- src/main.cpp
+++ src/main.cpp
@@ -7,19 +7,18 @@
 
 #include <iostream>
 
-const char *vertexShaderSource = "#version 330 core\n"
+const char *vertexShaderSource = "#version 410 core\n"
     "layout (location = 0) in vec3 aPos;\n"
     "void main()\n"
     "{\n"
     "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
     "}\0";
-const char *fragmentShaderSource = "#version 330 core\n"
+const char *fragmentShaderSource = "#version 410 core\n"
     "out vec4 color;\n"
     "void main()\n"
     "{\n"
-    // "   double coucou_d = 1.0;\n" // Doesn't work if uncommented
-    "   float coucou_f = 1.f;\n"
-    "   color = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "   double coucou = 0.5;\n"
+    "   color = vec4(1.0f, coucou, 0.2f, 1.0f);\n"
     "}\n\0";
 
 
